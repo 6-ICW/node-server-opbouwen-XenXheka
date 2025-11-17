0ece794e9b7da20d5ed4776bf5b0d54d38fa2af7
Xen <Xen.Xheka@student.tsaam.be>
2025-11-17 12:37:55 +0100

    versie 1

---
+++ controllers/cntrl.js
@@ -0,0 +1,10 @@
+const welkom = (req,res) => {
+    res.send("Welkom")
+}
+
+
+
+
+module.exports = {
+    welkom,
+}
---
+++ routes/leerlingen.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const routes = express.Router();
+const cntrlLeerling = require("../controllers/cntrl")
+
+routes.get("/",)
